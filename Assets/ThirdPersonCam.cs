f0f49445683be491ae22d756a2204e7ceb355cb2
Glenn Pierce <glennpierce@gmail.com>
2024-04-07 11:44:06 +0100

    Basic movement

--- Assets/ThirdPersonCam.cs
+++ Assets/ThirdPersonCam.cs
@@ -39,6 +39,7 @@ public class ThirdPersonCam : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha2)) SwitchCameraStyle(CameraStyle.Combat);
         if (Input.GetKeyDown(KeyCode.Alpha3)) SwitchCameraStyle(CameraStyle.Topdown);
 
+        
         // rotate orientation
         Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
         orientation.forward = viewDir.normalized;
